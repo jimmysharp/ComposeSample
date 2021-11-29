ab5a716d084dd13f3784c2b0fc618cd90051a2d6
sharp <jimmy.sharptech@gmail.com>
2021-11-30 01:43:11 +0900

    Drawerを追加

---
+++ app/src/main/java/com/example/composesample/ui/components/MainDrawer.kt
@@ -0,0 +1,80 @@
+package com.example.composesample.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composesample.ui.theme.MainTheme
+
+sealed class DrawerItem(val title: String) {
+    object Home : DrawerItem("Home")
+    object Settings : DrawerItem("Settings")
+}
+
+@Composable
+fun MainDrawer(
+    modifier: Modifier = Modifier,
+    navigateToHome: () -> Unit,
+    navigateToSettings: () -> Unit
+) {
+    MainDrawerRaw(
+        modifier = modifier,
+        navigateToHome = navigateToHome,
+        navigateToSettings = navigateToSettings
+    )
+}
+
+@Composable
+fun MainDrawerRaw(
+    modifier: Modifier = Modifier,
+    navigateToHome: () -> Unit = {},
+    navigateToSettings: () -> Unit = {}
+) {
+    Column(
+        modifier
+            .fillMaxSize()
+            .padding(start = 24.dp, top = 48.dp)
+            .background(MaterialTheme.colors.background),
+    ) {
+        DrawerColumnItem(
+            item = DrawerItem.Home,
+            selected = false,
+            onItemClick = { navigateToHome() })
+        DrawerColumnItem(
+            item = DrawerItem.Settings,
+            selected = false,
+            onItemClick = { navigateToSettings() })
+    }
+}
+
+@Composable
+fun DrawerColumnItem(item: DrawerItem, selected: Boolean, onItemClick: (DrawerItem) -> Unit) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onItemClick(item) }
+            .height(45.dp)
+            .padding(start = 10.dp)
+    ) {
+        Text(
+            text = item.title,
+            fontSize = 18.sp
+        )
+    }
+}
+
+@Preview
+@Composable
+fun MainDrawerPreview() {
+    MainTheme {
+        MainDrawerRaw()
+    }
+}
--- app/src/main/java/com/example/composesample/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/composesample/ui/screens/home/HomeScreen.kt
@@ -1,18 +1,18 @@
 package com.example.composesample.ui.screens.home
 
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import com.example.composesample.ui.components.MainAppBar
+import com.example.composesample.ui.components.MainDrawer
 import com.example.composesample.ui.screens.home.HomeViewModel
 import com.example.composesample.ui.theme.MainTheme
+import kotlinx.coroutines.launch
 
 @Composable
 fun HomeScreen(
@@ -20,24 +20,45 @@ fun HomeScreen(
     viewModel: HomeViewModel
 ) {
     HomeScreenRaw(
+        navigateToHome = {},
         navigateToSettings = navigateToSettings
     )
 }
 
 @Composable
 fun HomeScreenRaw(
+    navigateToHome: () -> Unit = {},
     navigateToSettings: () -> Unit = {}
 ) {
+    val scaffoldState = rememberScaffoldState(rememberDrawerState(DrawerValue.Closed))
+    val scope = rememberCoroutineScope()
     Scaffold(
+        scaffoldState = scaffoldState,
         topBar = {
             MainAppBar(
                 title = { Text(text = "Home") },
-                navigationIcon = { IconButton(onClick = {}) {
-                    Icon(Icons.Default.Menu, contentDescription = "Menu")
-                } },
-                actions = { IconButton(onClick = { navigateToSettings() }) {
-                    Icon(Icons.Default.Settings, contentDescription = "Settings")
-                } }
+                navigationIcon = {
+                    IconButton(onClick = { scope.launch { scaffoldState.drawerState.open() } }) {
+                        Icon(Icons.Default.Menu, contentDescription = "Menu")
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { navigateToSettings() }) {
+                        Icon(Icons.Default.Settings, contentDescription = "Settings")
+                    }
+                }
+            )
+        },
+        drawerContent = {
+            MainDrawer(
+                navigateToHome = {
+                    navigateToHome()
+                    scope.launch { scaffoldState.drawerState.close() }
+                },
+                navigateToSettings = {
+                    navigateToSettings()
+                    scope.launch { scaffoldState.drawerState.close() }
+                }
             )
         }
     ) {
